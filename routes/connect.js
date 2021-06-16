249ea5a3cfa5eedb60ba91dc590c580eafb5a3b8
Benjamin HARTMANN <benjaminhartmann.contact@gmail.com>
2021-06-16 13:46:07 +0200

    connect post

--- routes/connect.js
+++ routes/connect.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userControllers');
 
-router.get('/', userController.connectUser);
+router.post('/', userController.connectUser);
 
 module.exports = router;
